597dbd7d3225b55ab43c56eba8837001f976f6ca
Oleg2Pi <polikarpovoleg115@gmail.com>
2024-12-01 12:00:29 +0300

    Create constraints for a class "Product"

--- README.md
+++ README.md
@@ -9,13 +9,23 @@
       1. spring-boot-maven-plugin
       2. lombok
 2. Commit - Create directory "entity" and class "Product": 
-   1. add annotation in class Product from lombok:
+   1. use annotation for class Product from lombok:
       1. @Data
       2. @NoArgsConstructor
       3. @AllArgsConstructor
-   2. add fields in class Product:
+   2. create fields in class Product:
       1. name
       2. description
       3. price
       4. inStock
-3. 
+3. Commit - Create constraints for a class "Product":
+   1. add dependency in pom.xml:
+      1. spring-boot-starter-validation
+   2. use annotation from jakarta.validation.constraints
+      1. @NotBlank - field is not null
+      2. @Size - min and max size for field
+      3. @DecimalMin - min value for field
+   3. price = 0 by default
+   4. inStock = false by default
+   5. add annotation @Builder
+4. 
--- src/main/java/by/polikarpov/entity/Product.java
+++ src/main/java/by/polikarpov/entity/Product.java
@@ -1,16 +1,28 @@
 package by.polikarpov.entity;
 
+import jakarta.validation.constraints.DecimalMin;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
+@Builder
 public class Product {
 
+    @NotBlank(message = "Название товара обязательно.")
+    @Size(max = 255, message = "Название товара не должно превышать 255 символов.")
     private String name;
+
+    @Size(max = 4096, message = "Описание товара не должно превышать 4096 символов.")
     private String description;
-    private double price;
-    private boolean inStock;
+
+    @DecimalMin(value = "0", message = "Цена товара не может быть меньше 0.")
+    private double price = 0;
+
+    private boolean inStock = false;
 }
